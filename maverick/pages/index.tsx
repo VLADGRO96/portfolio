daeead22b6b40c024d0fed2a75dc92ea3b1c2057
Vladislav <groha.vlad@gmail.com>
2024-09-21 10:51:15 +0300

    add animation on home page

--- maverick/pages/index.tsx
+++ maverick/pages/index.tsx
@@ -6,6 +6,7 @@ import Stack from "@/components/Stack";
 
 //animation
 import { motion } from "framer-motion";
+import { smoothIn } from "../animate";
 
 export default function Home() {
 	return (
@@ -20,16 +21,34 @@ export default function Home() {
 				<div className="bg-[#DBD7FB] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-33rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
 				<div className="w-full h-full">
 					<div>
-						<div className="mb-4 lg:mt-48 mt-28">
+						<motion.div
+							variants={smoothIn("down", 0.2)}
+							initial="hidden"
+							animate="show"
+							exit="hidden"
+							className="mb-4 lg:mt-48 mt-28"
+						>
 							<Stack />
-						</div>
+						</motion.div>
 						{/* Heading  */}
-						<h1 className="h1">
+						<motion.h1
+							variants={smoothIn("down", 0.2)}
+							initial="hidden"
+							animate="show"
+							exit="hidden"
+							className="h1"
+						>
 							Book. Perfect. {""}
 							<span className="text-Red">Ride.</span>
-						</h1>
+						</motion.h1>
 						{/* SubTitle */}
-						<p className="text-Black mx-auto max-w-xl flex justify-center">
+						<motion.p
+							variants={smoothIn("down", 0.3)}
+							initial="hidden"
+							animate="show"
+							exit="hidden"
+							className="text-Black mx-auto max-w-xl flex justify-center"
+						>
 							Car Dealer is the most enticing, creative, modern and multipurpose
 							auto dealer Premium WordPress Theme. Suitable for any car dealer
 							websites, business or corporate websites. Express your style on
@@ -37,22 +56,35 @@ export default function Home() {
 							personalized choices for every journey. Enhance visuals with
 							interactive points, providing details and engaging storytelling
 							within your images effortlessly.
-						</p>
+						</motion.p>
 
 						{/* Button */}
-						<div className="mt-8 flex gap-x-4 justify-center z-10">
+						<motion.div
+							variants={smoothIn("down", 0.6)}
+							initial="hidden"
+							animate="show"
+							exit="hidden"
+							className="mt-8 flex gap-x-4 justify-center z-10"
+						>
 							<button className="flex-none rounded-full bg-Red px-6 py-2.5 text-sm font-normal hover:bg-Black transition-all duration-300 text-white">
 								View Details
 							</button>
 							<button className="flex-none rounded-full bg-Red px-6 py-2.5 text-sm font-normal hover:bg-Black transition-all duration-300 text-white">
 								Rent Now
 							</button>
-						</div>
+						</motion.div>
 
 						<div className="mt-20 w-full h-full">
-							<div className="w-full h-full flex justify-center">
+							<motion.div
+								variants={smoothIn("left", 0.5)}
+								initial="hidden"
+								animate="show"
+								exit="hidden"
+								transition={{ duration: 1, ease: "easeInOut " }}
+								className="w-full h-full flex justify-center"
+							>
 								<Car />
-							</div>
+							</motion.div>
 						</div>
 					</div>
 				</div>

4b043a50ae1705372fa507a83eba21aab21522ae
Vladislav <groha.vlad@gmail.com>
2024-10-12 11:49:28 +0300

    Skills page done

--- maverick/components/Skills.tsx
+++ maverick/components/Skills.tsx
@@ -1,16 +1,17 @@
 import { SkillBarProps } from "@/Types";
-import React, { useRef,useState, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
+
 
 export default function Skills({ skills }: SkillBarProps) {
 	const ref = useRef<HTMLDivElement>(null);
-	const[visible, setVisible] = useState<boolean>(false);
+	const [visible, setVisible] = useState<boolean>(false);
 	const [progress, setProgress] = useState<number[]>(skills.map(() => 0));
 	const [animatedProgress, setAnimatedProgress] = useState<number[]>(
 		skills.map(() => 0)
 	);
 	useEffect(() => {
 		function onScroll() {
-			if(ref.current) {
+			if (ref.current) {
 				const { top, bottom } = ref.current.getBoundingClientRect();
 				const isVisible = top < window.innerHeight && bottom >= 0;
 				if (isVisible && !visible) {
@@ -22,33 +23,39 @@ export default function Skills({ skills }: SkillBarProps) {
 		window.addEventListener("scroll", onScroll);
 		onScroll();
 		return () => removeEventListener("scroll", onScroll);
-	}, [skills, visible]); 
+	}, [skills, visible]);
 	useEffect(() => {
 		const intervalId = setInterval(() => {
-			setAnimatedProgress((prevProgress) => 
-				 prevProgress.map((value, index) => {
+			setAnimatedProgress((prevProgress) =>
+				prevProgress.map((value, index) => {
 					const diff = progress[index] - value;
 					const step = Math.ceil(diff / 10);
 					return value + step;
-				 })
+				})
 			);
 		}, 100);
 		return () => {
-			clearInterval(intervalId)
-		}
-	}, [progress])
+			clearInterval(intervalId);
+		};
+	}, [progress]);
 
 	return (
 		<div>
-			<div>
+			<div ref={ref} className="grid md:grid-cols-1 gap-4 p-10">
 				{skills.map((skill, index) => (
-					<div key={skill.name}>
-						<div>
+					<div key={skill.name} className="mb-4">
+						<div className="flex justify-between items-center mb-2">
 							<span>{skill.name}</span>
-							<span>{animatedProgress[index]}%</span>
+							<span className="font-bold text-sm">{animatedProgress[index]}%</span>
 						</div>
 						<div className="relative w-full h-1 bg-Black rounded-full overflow-hidden">
-							<div className="absolute top-0 left-0 h-full bg-WhiteGray rounded-full" style={{ width: `${visible ? animatedProgress[index] : 0}%`, transition: "width 1s" }}></div>
+							<div
+								className="absolute top-0 left-0 h-full bg-WhiteGray rounded-full"
+								style={{
+									width: `${visible ? animatedProgress[index] : 0}%`,
+									transition: "width 1s",
+								}}
+							></div>
 						</div>
 					</div>
 				))}
--- maverick/pages/skilllevel.tsx
+++ maverick/pages/skilllevel.tsx
@@ -5,6 +5,10 @@ import Image from "next/image";
 import Skills from "@/components/Skills";
 import { skills } from "@/Data/Data";
 
+// Animation
+import { motion } from "framer-motion";
+import { smoothIn } from "@/animate";
+
 export default function Skilllevel() {
 	return (
 		<>
@@ -14,25 +18,54 @@ export default function Skilllevel() {
 
 			<Wrapper className="text-center mt-24 flex flex-col justify-center">
 				<div className="w-full h-full">
-					<div className="text-WhiteGray font-medium uppercase text-sm text-center">
+					<motion.div
+						className="text-WhiteGray font-medium uppercase text-sm text-center"
+						variants={smoothIn("down", 0.2)}
+						initial="hidden"
+						animate="show"
+						exit="hidden"
+						transition={{ duration: 1, ease: "easeInOut" }}
+					>
 						- Only the best
-					</div>
+					</motion.div>
 
-					<div className="font-semibold text-3xl mt-4 lg:mb-16 mb-8">
+					<motion.p
+						variants={smoothIn("down", 0.3)}
+						initial="hidden"
+						animate="show"
+						exit="hidden"
+						transition={{ duration: 1, ease: "easeInOut" }}
+						className="font-semibold text-3xl mt-4 lg:mb-16 mb-8 text-Black"
+					>
 						Our Satisfied Clients All Around The Globe
-					</div>
+					</motion.p>
 
 					<div className="flex h-full flex-col justify-center text-center">
 						<div className="flex flex-col gap-12 lg:flex-row items-center justify-center mb-12 lg:pt-0 pt-12">
-							<Image
-								src={"/skills.jpg"}
-								alt="Skills"
-								width={400}
-								height={500}
-							/>
-							<div>
+							<motion.div
+								variants={smoothIn("right", 0.5)}
+								initial="hidden"
+								animate="show"
+								exit="hidden"
+								transition={{ duration: 1, ease: "easeInOut" }}
+							>
+								<Image
+									src={"/skills.jpg"}
+									alt="Skills"
+									width={400}
+									height={500}
+								/>
+							</motion.div>
+
+							<motion.div
+								variants={smoothIn("left", 0.5)}
+								initial="hidden"
+								animate="show"
+								exit="hidden"
+								transition={{ duration: 1, ease: "easeInOut" }}
+							>
 								<Skills skills={skills} />
-							</div>
+							</motion.div>
 						</div>
 					</div>
 				</div>
